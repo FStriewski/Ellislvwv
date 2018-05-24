0551cd1d2a51e9b2bc185846fafb13fb4a4aff4b
FStriewski <fstriewski@outlook.com>
2018-05-24 14:26:05 +0200

    Add EventItem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import EventItem from './components/carousel/EventItem'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <EventItem/>
       </div>
     );
   }
---
+++ src/components/carousel/EventItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+
+export default class EventItem extends React.Component {
+
+    render(){
+        return (
+            <div>empty</div>
+        )
+    }
+
+}
+
+// export default EventItem
