8d6b584d05e539992543229b112e6a32728a09e7
FStriewski <fstriewski@outlook.com>
2018-05-24 15:12:07 +0200

    Dispatch fetchAll action to API successfully

--- src/actions/events.js
+++ src/actions/events.js
@@ -1,7 +1,18 @@
+import * as request from 'superagent'
+
+const baseUrl = 'https://voorhoede-colibri-api-zhmoaomjvy.now.sh/api/v1'
+
+
 export const FETCH_ALL_EVENTS = "FETCH_ALL_EVENTS"
 
 
-export const fetchAllEvents = () => ({
-    type: FETCH_ALL_EVENTS,
-    payload: null
-})
+export const fetchAllEvents = () => (dispatch) => {
+    request
+        .get(`${baseUrl}/events?language=en`)
+        .then(response => dispatch({
+            type: FETCH_ALL_EVENTS,
+            payload: response.body
+        }))
+
+
+}
